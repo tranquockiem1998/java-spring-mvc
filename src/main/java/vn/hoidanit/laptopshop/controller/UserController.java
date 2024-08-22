4c9debc01cffad2fa6ea1202fe771bbf3efb3c53
Ngo Chau <chau78840@gmail.com>
2024-08-23 06:57:42 +0700

    video 48

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,26 +7,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-    
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
-    
+
     private UserService userService;
     
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    @GetMapping("")
+    
+    @RequestMapping("/hoidanit")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "eric.html";
     }
 }
+
+// @RestController
+// public class UserController {
+
+//     private UserService userService;
+    
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("")
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+//     }
+// }
