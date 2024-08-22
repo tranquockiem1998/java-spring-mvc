0b413beaddbb856b51fdd75073a89057a780c98b
Ngo Chau <chau78840@gmail.com>
2024-08-23 06:29:47 +0700

    video 46

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,8 +2,11 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+// include >< exclude 
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
