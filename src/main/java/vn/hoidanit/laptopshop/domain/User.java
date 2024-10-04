ac0df6f888766b92d23e0b5c0a6dd8f3359d7b05
Ngo Chau <chau78840@gmail.com>
2024-10-04 09:16:55 +0700

    video 102

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -11,8 +11,8 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -22,15 +22,15 @@ public class User {
     private long id;
 
     @NotNull
-    @Email
+    @Email(message = "Email không hợp lệ", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
 
     @NotNull
-    @Min(2)
+    @Size(min = 2, message = "Password phải có tối thiểu 2 ký tự")
     private String password;
 
     @NotNull
-    @Min(2)
+    @Size(min = 3, message = "Full name phải có tối thiểu 3 ký tự")
     private String fullName;
 
     private String address;
--- src/main/java/vn/hoidanit/laptopshop/service/UploadService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UploadService.java
@@ -21,6 +21,8 @@ public class UploadService {
     }
 
     public String handleSaveUploadFile(MultipartFile file, String targetFolder) {
+        if (file.isEmpty())
+            return "";
         String rootPath = this.servletContext.getRealPath("/resources/images");
         String finalName = "";
         try {
