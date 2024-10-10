650cd5fc83b8bf8a014cce1da715c6ea10503f03
Ngo Chau <chau78840@gmail.com>
2024-10-10 13:55:35 +0700

    video 113

--- src/main/java/vn/hoidanit/laptopshop/domain/DTO/RegisterDTO.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/DTO/RegisterDTO.java
@@ -1,13 +1,21 @@
 package vn.hoidanit.laptopshop.domain.DTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import vn.hoidanit.laptopshop.service.validator.RegisterChecked;
 
 @RegisterChecked // Annotation tự định nghĩa
 public class RegisterDTO {
+
+    @Size(min = 3, message = "FirstName phải có tối thiểu 3 ký tự")
     private String firstName;
     private String lastName;
+
+    @Email(message = "Email không hợp lệ", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
     private String password;
+
+    @Size(min = 3, message = "Confirm password phải có tối thiểu 3 ký tự")
     private String confirmPassword;
 
     public String getFirstName() {
