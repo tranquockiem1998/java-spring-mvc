4243e1395dffb8358958265c6b78fad426694b13
Ngo Chau <chau78840@gmail.com>
2024-11-22 17:50:19 +0700

    video 131

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -107,8 +107,17 @@ public class ItemController {
     public String handlePlaceOrder(HttpServletRequest request, @RequestParam("receiverName") String receiverName,
             @RequestParam("receiverAddress") String receiverAddress,
             @RequestParam("receiverPhone") String receiverPhone) {
+        User currentUser = new User();
         HttpSession session = request.getSession(false);
-        return "redirect:/";
+        long id = (long) session.getAttribute("id");
+        currentUser.setId(id);
+        this.productService.handlePlaceOrder(currentUser, session, receiverName, receiverAddress, receiverPhone);
+        return "redirect:/thankyou";
+    }
+
+    @GetMapping("/thankyou")
+    public String getThankYouPage(Model model) {
+        return "client/cart/thankyou";
     }
 
 }
